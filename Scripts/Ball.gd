3f36bf86a556fb9e7e959174ed1bc4f88b9647cb
Emily Clinkscales <emclinks@github.com>
2020-02-24 17:32:09 -0500

    Day 1 Pre-class changes

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -3,9 +3,23 @@ extends RigidBody2D
 onready var Game = get_node("/root/Game")
 onready var Starting = get_node("/root/Game/Starting")
 
+var _max_offset = 4
+var _decay_rate = 0.0
+var _start_size
+var _start_position
+var _trauma = 0.0
+var _rotation = 0
+var _rotation_speed = 0.05
+var _color = 0.0
+var _color_decay = 1
+var _normal_color
+
 func _ready():
 	contact_monitor = true
 	set_max_contacts_reported(4)
+	_start_position = $ColorRect.rect_position
+	_start_size = $ColorRect.get_transform().size
+	_normal_color = $ColorRect.color
 
 func _physics_process(delta):
 	# Check for collisions
@@ -19,3 +33,20 @@ func _physics_process(delta):
 		Game.change_lives(-1)
 		Starting.startCountdown(3)
 		queue_free()
+		
+
+func add_trauma(amount):
+	_trauma = min(_trauma + amount, 1)
+
+func _decay_trauma(delta):
+	var change = _decay_rate * delta
+	_trauma = max(_trauma - change, 0)
+
+func _apply_shake():
+	var shake = _trauma * _trauma
+	var 0_x = _max_offset * shake * _get_neg_or_pos_scalar()
+	var 0_y = _max_offset * shake * _get_neg_or_pos_scalar()
+	$ColorRect.rect_position = _start_position * Vector2(0_x, 0_y)
+
+func _get_neg_or_pos_scalar():
+	return rand_range(-1,0, 1,0)
